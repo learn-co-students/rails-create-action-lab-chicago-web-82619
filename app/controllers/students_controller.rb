bf58cee2de00b8b06d779c819d7320b0d503f26c
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-09-23 11:22:54 -0500

    lab complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    p params
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    @student = Student.create(first_name: first_name, last_name: last_name, created_at: DateTime.now)
+    redirect_to student_path(@student)
   end
 
 end
